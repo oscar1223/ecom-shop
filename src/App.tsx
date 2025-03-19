d645779b3b99893f35749aa1ccfcac334e151567
oscar1223 <oscararauzp@gmail.com>
2025-03-19 09:08:46 +0100

    responsive fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // src/App.tsx
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
